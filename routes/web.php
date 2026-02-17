7079bdefe0ddd328e26004e65791dd2a305b4907
Alex Madrid <alexmadrid326@gmail.com>
2026-02-16 20:05:07 -0400

    enrutamiento

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//Ruta basica para mostrar una vista 'return view()'
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta basica para mostrar un texto
+Route::get('/saludo', function () {
+    return 'HOLA MUNDO!';
+});
+
+Route::get('/usuario/create', function () {
+    return "Usuario creado exitosamente";
+});
+
+
+//Ruta con parametros para mostrar datos variables desde la uri
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuario: {$id}";
+});
+
+//Ruta con multiples parametros para mostrar datos variables desde la uri
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return "Post: {$postId}, Comentario: {$commentId}";
+});
+
+//Ruta con multiples parametros y condicionales para mostrar datos variables desde la uri
+Route::get('/posts/{post?}/comments/{comment?}', function ($postId, $commentId = null) {
+    if ($commentId) {
+        return "<h1>Post: {$postId}, Comentario: {$commentId}</h1>";
+    }else{
+        return "<h1>Post: {$postId}, Comentario desconocido</h1>";
+    }
+});
+
+
+
+
+?>
